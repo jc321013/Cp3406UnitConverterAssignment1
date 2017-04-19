fa6dc79734e3716c3253ca68ef50b95bad4acfd7
jc321013 <jared.marcolongo@my.jcu.edu.au>
2017-04-19 23:04:08 +1000

    jhh

--- app/src/main/java/com/example/jc321013/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/jc321013/unitconverter/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.jc321013.unitconverter;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
-//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
